3e342ae7b86d02c75c44a6f8176f81cc3b9d8b7b
eco-metric <mohmedadel392@gmail.com>
2024-11-19 19:30:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2278,6 +2278,8 @@ function calculateTotalResults_Window(event) {
 
 // حساب القيم في آخر صف الخاص بالـ Total Impact
 document.getElementById("btnTtotalImpact").addEventListener("click", function () {
+      event.preventDefault();
+
     // Function to calculate the total for a given set of IDs
     function calculateTotal(impactId, resultIds) {
         let total = 0;
